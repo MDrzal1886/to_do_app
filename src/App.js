a32fb94a46ff3f37a588746c9de7e1a5b93a160f
Mateusz Drzał <mdrzal1886@gmail.com>
2021-11-08 23:16:14 +0100

    fix orientation ios issue v6 i hope last

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import Navigation from "./components/Navigation";
 
 function App() {
+  window.onresize = () => {
+    document.body.height = window.innerHeight;
+    document.body.width = window.innerWidth;
+  };
+  window.onresize();
   return (
     <div className="container">
       <h1>Manager zadań</h1>
